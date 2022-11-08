917e820b7e3eae242c564cc6cfdcd2e890c78290
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-11-07 21:52:52 -0300

    first commit

---
+++ mock_database.js
@@ -0,0 +1,20 @@
+var db = {
+    dados: [
+        {
+            nome:'Juliana',
+            sobrenome: 'costa',
+            email: 'juli@gmail.com',
+            nomeprod: 'Carrinho de bebe',
+            precoprod: 350.00,
+            descricao: 'Carrinho de bebe com capa de proteção de chuva'
+        },
+        {
+        nome:'Manuela',
+        sobrenome: 'firmino',
+        email: 'manu@gmail.com',
+        nomeprod: 'bolsa de bebe',
+        precoprod: 78.00,
+        descricao: 'Bolsa de 20 litros'
+    },
+    ]
+}

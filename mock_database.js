abeb7ec2cea03843eff3e286a6d4eaf8752386e8
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-11-15 22:00:30 -0300

    first commit

--- mock_database.js
+++ mock_database.js
@@ -17,61 +17,6 @@ var db = {
             image: 'http://lorempixel.com.br/200/120/food/2',
             precoprod: 78.00,
             descricao: 'Bolsa de 20 litros'
-        },
-        {
-            nome:'Manuela',
-            sobrenome: 'firmino',
-            email: 'manu@gmail.com',
-            nomeprod: 'bolsa de bebe',
-            image: 'http://lorempixel.com.br/200/120/food/3',
-            precoprod: 82.28,
-            descricao: 'Bolsa de 20 litros'
-        },
-        {
-            nome:'Manuela',
-            sobrenome: 'firmino',
-            email: 'manu@gmail.com',
-            nomeprod: 'bolsa de bebe',
-            image: 'http://lorempixel.com.br/200/120/food/4',
-            precoprod: 82.28,
-            descricao: 'Bolsa de 20 litros'
-        },
-        {
-            nome:'Manuela',
-            sobrenome: 'firmino',
-            email: 'manu@gmail.com',
-            nomeprod: 'bolsa de bebe',
-            image: 'http://lorempixel.com.br/200/120/food/5',
-            precoprod: 82.28,
-            descricao: 'Bolsa de 20 litros'
-        },
-        {
-            nome:'Manuela',
-            sobrenome: 'firmino',
-            email: 'manu@gmail.com',
-            nomeprod: 'bolsa de bebe',
-            image: 'http://lorempixel.com.br/200/120/food/6',
-            precoprod: 82.28,
-            descricao: 'Bolsa de 20 litros'
-        },
-        {
-            nome:'Manuela',
-            sobrenome: 'firmino',
-            email: 'manu@gmail.com',
-            nomeprod: 'bolsa de bebe',
-            image: 'http://lorempixel.com.br/200/120/food/6',
-            precoprod: 82.28,
-            descricao: 'Bolsa de 20 litros'
-        },
-        {
-            nome:'Manuela',
-            sobrenome: 'firmino',
-            email: 'manu@gmail.com',
-            nomeprod: 'bolsa de bebe',
-            image: 'http://lorempixel.com.br/200/120/food/6',
-            precoprod: 82.28,
-            descricao: 'Bolsa de 20 litros'
-        },
-        
+        },       
     ]
 }
